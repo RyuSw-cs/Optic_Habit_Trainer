20e2d1332eaf24c7c69dcb6b18cc84e5d721a59a
RyuSw-cs <rsw1452@naver.com>
2021-12-05 04:22:58 +0900

    feat: #2 Modifiy Camera Preview

--- app/src/main/java/com/oht/UI/SecondStep/Ready/SecondStepActivity.java
+++ app/src/main/java/com/oht/UI/SecondStep/Ready/SecondStepActivity.java
@@ -1,16 +1,28 @@
 package com.oht.UI.SecondStep.Ready;
 
 import android.content.Intent;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CaptureRequest;
+import android.media.ImageReader;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.util.Size;
+import android.view.TextureView;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.oht.R;
 import com.oht.UI.Train.TrainBackground;
 import com.oht.UI.SecondStep.Result.SecondStepEndActivity;
+
+import java.util.concurrent.Semaphore;
 
 public class SecondStepActivity extends AppCompatActivity {
 
