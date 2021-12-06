b9790827654d8e7ab3e8e5be6030b271d8a13108
RyuSw-cs <rsw1452@naver.com>
2021-12-07 03:46:59 +0900

    feat: #2  Create MediaRecorder

--- app/src/main/java/com/oht/UI/FirstStep/Ready/FirstStepActivity.java
+++ app/src/main/java/com/oht/UI/FirstStep/Ready/FirstStepActivity.java
@@ -1,17 +1,30 @@
 package com.oht.UI.FirstStep.Ready;
 
 import android.content.Intent;
+import android.hardware.Camera;
+import android.media.CamcorderProfile;
+import android.media.MediaRecorder;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.TextureView;
 import android.widget.ProgressBar;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.oht.R;
 import com.oht.UI.FirstStep.Result.FirstStepEndActivity;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -20,18 +33,28 @@ public class FirstStepActivity extends AppCompatActivity {
     private int time = 15;
     private int proceed = 7;
     private ProgressBar progressBar;
+    private MediaRecorder mediaRecorder;
+    private File fileName, fileDir;
+    private SurfaceView surfaceView;
+    private SurfaceHolder surfaceHolder;
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_step);
-        init();
+        try {
+            init();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        final Handler handler = new Handler(){
-            public void handleMessage(Message msg){
+        final Handler handler = new Handler() {
+            public void handleMessage(Message msg) {
                 time--;
                 proceed += 7;
-                if(time == 0){
+                if (time == 0) {
+                    stopRecording();
                     Intent intent = new Intent(getApplicationContext(), FirstStepEndActivity.class);
                     startActivity(intent);
                     finish();
@@ -52,9 +75,59 @@ public class FirstStepActivity extends AppCompatActivity {
         timer.schedule(timerTask, 0, 1000);
     }
 
-    private void init(){
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void init() throws IOException {
         progressBar = findViewById(R.id.first_step_progress_bar);
         progressBar.setIndeterminate(false);
         progressBar.setProgress(0);
+
+        surfaceView = findViewById(R.id.surfaceView);
+        surfaceHolder = surfaceView.getHolder();
+        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+        long now = System.currentTimeMillis();
+        Date date = new Date(now);
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd_HHmmss");
+
+        fileDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS), "Optic_Habit_Training");
+        if (!fileDir.exists()) {
+            fileDir.mkdirs();
+        }
+        fileName = new File(fileDir.getPath() + "/" + simpleDateFormat.format(date) + ".mp4");
+        recordAudio();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void recordAudio(){
+        mediaRecorder = new MediaRecorder();
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mediaRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT);
+        mediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.DEFAULT);
+        mediaRecorder.setOutputFile(fileName);
+        mediaRecorder.setPreviewDisplay(surfaceHolder.getSurface());
+        try {
+            mediaRecorder.prepare();
+            mediaRecorder.start();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mediaRecorder != null) {
+            mediaRecorder.release();
+            mediaRecorder = null;
+        }
+    }
+    public void stopRecording(){
+        if (mediaRecorder != null) {
+            mediaRecorder.stop();
+            mediaRecorder.release();
+            mediaRecorder = null;
+        }
     }
 }
